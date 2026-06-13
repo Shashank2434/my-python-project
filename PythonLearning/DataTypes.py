f3c9438b90cb43ccff4c1967d4c7a6ed742d7079
Shashank2434 <90554594+Shashank2434@users.noreply.github.com>
2026-06-13 23:57:03 +0530

    Add files via upload

---
+++ PythonLearning/DataTypes.py
@@ -0,0 +1,46 @@
+# In python, a data is a classification that specifies the type of value a variable can hold. we can check data type using type() function
+# my_name=("shashank")
+# print(type(my_name))
+
+# Basic DataTypes in python
+# 1.Numeric:
+     # Integer(Whole number) ,  , complex
+        # Quantity=55
+        # print(f"I like to buy {Quantity} escorts")
+     # Float(Decimal portion)
+        # price=10.99
+        # print(f"The price of rice is {price} inr")
+     # Complex(one part is imaginary(let))
+        # a3 = complex(3,5)
+        # print(type(a3))
+# 2.Sequence:
+    # String (It can be with single quotes or double quotes)
+        # Website='Dash@slash.com'
+        # print(f'My Website is {Website}')
+    # List
+        # A=[1,2,3,'A','B','C',"shashank"]
+        # print(type(A))
+    # Tuple
+        # A=(1,2,3,'A','B','C',"shashank")
+        # print(type(A))
+# 3.Dictionary(Key,value)
+    # my_dictionary= {'name':'shashank','age':29}
+        # print(my_dictionary)
+        # print(type(my_dictionary))
+# 4.Sets (combination of object)
+    # my_set ={1,2,3,'A','B','C',"shashank"}
+    # print(my_set)
+# 5.Boolean(True or False) first letter is capital
+    # online= False
+    # print(f"Are you online ?{online}")
+
+    # if online:
+    #     print("I am online")
+    # else:
+    #     print("User is offline")
+
+# 6.Binary:Bytes,Bytearray,memoryview
+    # Binary=b"shashank"
+    # print(Binary)
+
+
---
+++ PythonLearning/Functions.py
@@ -0,0 +1,57 @@
+# def my_function (param)
+#     instruction1
+#     instruction1
+#
+# return result
+
+
+# Type of function
+# 1. Builtin library function:
+# These are standard function in python that are available to use
+# Examples: print(),input(),type(),sum(),max(), etc
+# 2.User Defined function
+# We can create our own function based on our requirement
+
+# Create function without parameter
+    # def greetings():
+    #     print("Hello world")
+    # call function
+    #     greetings()
+
+
+# Create function with parameter
+# def add2number(a,b): #parameter
+#     result=a+b
+#     print("The sum is :", result)
+#
+# add2number(a=2001,b=512) #5,3 argument
+#
+# def add3number(a,b,c): #parameter
+#     result=a+b+c
+#     print("The sum is :", result)
+#
+# add3number(a=2001,b=512,c=40023) #5,3 argument
+
+#function with return statement
+# def add2number(a,b):
+#     return a+b
+#
+# result=add2number(122,232)
+# print(result)
+
+#function to convert celsius to fahrenheit
+# def celsius_to_fahrenheit(celsius):
+#     fahrenheit = (celsius * 9/5) +32
+#     return fahrenheit
+# temp_f=celsius_to_fahrenheit(25)
+# print(int(temp_f))
+
+# The pass Statement
+# It is a placeholder in a function or loop. It does nothing and is used when you need to write code that will be added later or to define an empty function
+
+# def kuchbhi():
+#     pass
+# print("Shashank")
+
+
+
